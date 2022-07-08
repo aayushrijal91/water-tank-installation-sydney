a5058685b0bd5a340e8ee4bc1647e775bd8c3190
Aayush Rijal <aayush.rijal99@gmail.com>
2022-07-08 16:38:28 +1000

    Why choose section done

--- app/src/main.php
+++ app/src/main.php
@@ -105,4 +105,247 @@ include __DIR__ . '/header.php';
     </div>
 </section>
 
+<section class="products">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-11">
+                <div class="text-center h3 fw-800 pb-5">Want to see some of our products?</div>
+                <div class="row g-3 pt-6">
+                    <div class="col-md-4 col-xl-2">
+                        <div class="product-card">
+                            <?= renderImg("under-deck-tank.png", "lib") ?>
+                            <div class="info">
+                                <div>
+                                    <div class="text-primary h17 fw-700 line-height-4">Under Deck Tank</div>
+                                    <div class="text-white h16 fw-900 py-2">from $99</div>
+                                </div>
+                                <a class="btn btn-block text-light-grey fw-500 h27">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xl-2">
+                        <div class="product-card">
+                            <?= renderImg("slim-tank.png", "lib") ?>
+                            <div class="info">
+                                <div>
+                                    <div class="text-primary h17 fw-700 line-height-4">Slim Tank</div>
+                                    <div class="text-white h16 fw-900 py-2">from $99</div>
+                                </div>
+                                <a class="btn btn-block text-light-grey fw-500 h27">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xl-2">
+                        <div class="product-card">
+                            <?= renderImg("submersible.png", "lib") ?>
+                            <div class="info">
+                                <div>
+                                    <div class="text-primary h17 fw-700 line-height-4">Submersible Pump &amp; Accessories</div>
+                                    <div class="text-white h16 fw-900 py-2">from $99</div>
+                                </div>
+                                <a class="btn btn-block text-light-grey fw-500 h27">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xl-2">
+                        <div class="product-card">
+                            <?= renderImg("garden-pump.png", "lib") ?>
+                            <div class="info">
+                                <div>
+                                    <div class="text-primary h17 fw-700 line-height-4">Garden Pump</div>
+                                    <div class="text-white h16 fw-900 py-2">from $99</div>
+                                </div>
+                                <a class="btn btn-block text-light-grey fw-500 h27">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xl-2">
+                        <div class="product-card">
+                            <?= renderImg("external-diverter.png", "lib") ?>
+                            <div class="info">
+                                <div>
+                                    <div class="text-primary h17 fw-700 line-height-4">External Diverter Pump</div>
+                                    <div class="text-white h16 fw-900 py-2">from $99</div>
+                                </div>
+                                <a class="btn btn-block text-light-grey fw-500 h27">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-xl-2">
+                        <div class="product-card">
+                            <?= renderImg("concrete-tank.png", "lib") ?>
+                            <div class="info">
+                                <div>
+                                    <div class="text-primary h17 fw-700 line-height-4">Concrete Tank</div>
+                                    <div class="text-white h16 fw-900 py-2">from $99</div>
+                                </div>
+                                <a class="btn btn-block text-light-grey fw-500 h27">Find Out More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="about-us">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-11">
+                <div class="row g-2">
+                    <div class="col-4 h5 fw-700">Who are we?</div>
+                    <div class="col-8 fw-600 fw-600 h13 line-height-3">
+                        A family owned and operated business, Aussie Water Savers (AWS) is a leading manufacturer & supplier & installer of Rainwater Tanks in Sydney.
+                    </div>
+                </div>
+                <div class="row justify-content-end pt-6">
+                    <div class="col-6">
+                        <p class="text-secondary fw-700 h18 pb-4">Fewer than 26% of households in Australia use a rainwater tank.</p>
+                        <p class="h24 line-height-2">Aussie Water Savers produces high quality, value for money, rainwater tank systems for construction professionals completing both residential and commercial projects.</p>
+                        <p class="h24 line-height-2">We have been providing our Rainwater Tank services and products business for over 35+ years. Helping households save over 1 million litres per year which equates to significant savings per household. Our experienced technicians can help you save the environment and on expenses, providing expertise whether it is your home or business.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="solutions">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10 font-proxima-nova fw-800 text-dark-grey h4 text-tertiary text-center">
+                We pride ourselves on offering cost effective, <span class="text-white">value for money solutions</span> for our customers.
+            </div>
+            <div class="text-white font-proxima-nova text-center h13 fw-700 pt-5">
+                We are the rainwater tank system specialists with the team & talent to fix problems no one else can
+            </div>
+            <div class="row justify-content-center gx-3 gy-6">
+                <div class="col-md-6 col-lg-3">
+                    <div class="solution-card">
+                        <?= renderImg("sol-1.png", "lib") ?>
+                        <div class="caption">
+                            <div class="fw-600 h25 pb-3 text-capitalize">Off Grid Water Supply Management</div>
+                            <div class="pt-1 h29">Just because you're in the city, doesn't mean you can't have your own off grid water supply. <span class="fw-800">Your home is the perfect place to capture and store your own water.</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="solution-card">
+                        <?= renderImg("sol-2.png", "lib") ?>
+                        <div class="caption">
+                            <div class="fw-600 h25 pb-3 text-capitalize">Full System replacement</div>
+                            <div class="pt-1 h29">It's time to say <span class="fw-800">goodbye to your old broken down system.</span> We can replace your old system for a new one.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="solution-card">
+                        <?= renderImg("sol-3.png", "lib") ?>
+                        <div class="caption">
+                            <div class="fw-600 h25 pb-3 text-capitalize">Stormwater management</div>
+                            <div class="pt-1 h29">Stormwater not flowing where it should? <span class="fw-800">Is your home being inundated with water even with light rain?</span> Speak to us about rectifying stormwater flow.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="solution-card">
+                        <?= renderImg("sol-4.png", "lib") ?>
+                        <div class="caption">
+                            <div class="fw-600 h25 pb-3 text-capitalize">Irrigation system installation</div>
+                            <div class="pt-1 h29">Do you want to put your <span class="fw-800">existing rainwater harvesting system to better use?</span> We can install irrigation systems to suit</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="solution-card">
+                        <?= renderImg("sol-5.png", "lib") ?>
+                        <div class="caption">
+                            <div class="fw-600 h25 pb-3 text-capitalize">Rainwater harvesting system installation</div>
+                            <div class="pt-1 h29">Is your home funnelling stormwater to the street? Time to start storing and <span class="fw-800">using that water for your benefit.</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="solution-card">
+                        <?= renderImg("sol-6.png", "lib") ?>
+                        <div class="caption">
+                            <div class="fw-600 h25 pb-3 text-capitalize">Tank system upgrade</div>
+                            <div class="pt-1 h29">Already have a tank system but want more out of it? We can <span class="fw-800">upgrade your existing tank system</span> to a new system or add to your existing one</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="solution-card">
+                        <?= renderImg("sol-7.png", "lib") ?>
+                        <div class="caption">
+                            <div class="fw-600 h25 pb-3 text-capitalize">Pump, filter & accessory upgrade & replacement</div>
+                            <div class="pt-1 h29">When it's time to <span class="fw-800">improve the way you access rainwater</span>, we have the solution to suit your needs.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="why-choose">
+    <div class="part-1">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-11">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-5"><?= renderImg("grey-tank.png", "lib") ?></div>
+                        <div class="col-7">
+                            <div class="h3 fw-700 line-height-4">Why Choose Pauls Tank Warehouse?</div>
+                            <div class="h13 fw-700 line-height-1 py-5">The average person consumes 124,100 litres of water per year!</div>
+                            <p class="line-height-2 h21">Water is fundamental to life and having an economical and accessible source is just as important. As veterans in the business, we understand whether you are replacing or installing a rainwater tank, you want to get it done right.</p>
+                            <p class="line-height-2 h21">We have been helping Australian communities with saving on their water bills since the beginning.</p>
+                            <a href="#form" class="btn btn-block text-secondary px-0 pt-4 h19 fw-700">
+                                <span class="pe-3">Free ONSITE Home Quotes</span> <svg width="30" height="14" viewBox="0 0 30 14" fill="#0A3DE2" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M28.7431 6.29768L23.0817 0.636262C22.7428 0.297492 22.2332 0.196167 21.7905 0.379522C21.3478 0.562876 21.0591 0.994815 21.059 1.47398V5.06183C21.059 5.22543 20.9264 5.35806 20.7628 5.35806H2.43017C1.44857 5.35806 0.652832 6.1538 0.652832 7.13539C0.652832 8.11699 1.44857 8.91273 2.43017 8.91273H20.7628C20.9264 8.91273 21.059 9.04535 21.059 9.20895V12.7968C21.0591 13.276 21.3478 13.7079 21.7905 13.8913C22.2332 14.0746 22.7428 13.9733 23.0817 13.6345L28.7431 7.97311C29.2056 7.51041 29.2056 6.76038 28.7431 6.29768Z" fill="#0A3DE2" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="part-2">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-11">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 pe-xl-5">
+                            <div class="h3 fw-700 line-height-4 pb-4">Why Choose Pauls Tank Warehouse?</div>
+                            <p class="line-height-2 h21 pt-3 text-dark-grey">
+                                The Building Sustainability Index (BASIX), is an index that was rolled out across New South Wales in 2004. The aim of this law was to make sure all residential houses and dwellings are more sustainable by including alternative water supplies such as rainwater harvesting or recycled wastewater. <sapn class="fw-700">In the long term incentivising for not using mains supply which can be harmful to our environment.</span>
+                            </p>
+                            <p class="line-height-2 h21 text-dark-grey">
+                                This new measure stipulates how much energy efficiency your home must achieve each year before you apply development approval; otherwise it won't pass inspection standards!
+                            </p>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="questions-card">
+                                <div class="h24 pb-4 fw-700">Am I Eligible?</div>
+                                <div class="h24 text-dark-grey">To be eligible for some rebates you may be required to have a tank connected to your toilet or laundry. Depending on your local council you will need to confirm what is claimable.</div>
+                            </div>
+                            <div class="questions-card">
+                                <div class="h24 pb-4 fw-700">How Much does it Cost?</div>
+                                <p class="h24 text-dark-grey">Am I Eligible?</p>
+                                <div class="h24 text-dark-grey">To be eligible for some rebates you may be required to have a tank connected to your toilet or laundry. Depending on your local council you will need to confirm what is claimable.</div>
+                            </div>
+                            <div class="questions-card">
+                                <div class="h24 pb-4 fw-700">How do I Apply?</div>
+                                <div class="h24 text-dark-grey">Download your RainWater Tank Rebate Application form. You can download it from here:<br> <a href="https://www.energy.gov.au/rebates" target="_blank" class="btn p-0 text-dark-grey h24">https://www.energy.gov.au/rebates</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php include __DIR__ . '/footer.php'; ?>
